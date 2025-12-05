e7f78d903385c3b02249e47a58a4f43e1dc2cc77
Logan Pendergrass <logan@ironhorse.io>
2025-12-05 15:10:34 -0800

    organize

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,18 +7,13 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     build: {
-        // build as a single IIFE (good for a simple browser bundle)
         lib: {
             entry: path.resolve(__dirname, 'src/tasks-and-queue.js'), // your app entry
             name: 'KFTasks', // global var when using IIFE
             fileName: () => 'bundle.min.js',
-            formats: ['iife']
+            formats: ['iife'] // build as a single IIFE (good for a simple browser bundle)
         },
         sourcemap: true,
         minify: 'esbuild',
-        rollupOptions: {
-            // mark externals if you don't want them bundled
-            external: []
-        }
     }
 });
